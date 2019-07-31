204d0b736bdc95100816dc9f133b514ed37fdfba
Niamke Dozier <s1niamke@gmail.com>
2019-07-31 01:06:23 -0500

    File Management

--- face.py
+++ face.py
@@ -1,9 +1,20 @@
+
+
 import numpy as np
 import cv2
+import pickle
+
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
-
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizers/face-trainner.yml")
 cap = cv2.VideoCapture(0)
+
+labels = {}
+with open("pickles/face-labels.pickle", 'rb') as f:
+        og_labels = pickle.load(f)
+        og_labels = {v:k for k,v in og_labels.items()}
+
 
 while(cap.isOpened()):
     #Capture the video by the frame
@@ -13,11 +24,13 @@ while(cap.isOpened()):
     #detect faces and scalefactor for capturing image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors = 8)
     for (x, y, w, h) in faces:
-        print(x, y, w, h)
+        #print(x, y, w, h)
         #Region of interest 
         roi_gray = gray[y:y+h, x:x+w] #(ycord start, ycord end) same with x cord 
         roi_color = frame[y:y+h, x:x+w]
         #Create new image
+        id_, conf = recognizer.predict(roi_gray)
+        
         img_item = "new_image.png"
         cv2.imwrite(img_item, roi_gray)
 
